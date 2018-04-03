92b03eb3be56ca050357356db076491979424d0c
Yathartha <tuladhay@oregonstate.edu>
2018-04-03 14:59:51 -0700

    more cleanup

--- rllab/Data_for_running_policy/Experiment2/plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-data = pd.read_csv("/home/yathartha/PycharmProjects/RL/cassierl/rllab/Data_for_running_policy/Experiment2/progress.csv")
-avg_return = data["AverageReturn"]
-iter = data["Iteration"]
-plt.plot(iter, avg_return)
-plt.show()
-
--- rllab/Data_for_running_policy/Experiment4/plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-data = pd.read_csv("progress.csv")
-avg_return = data["AverageReturn"]
-iter = data["Iteration"]
-plt.plot(iter, avg_return)
-plt.show()
-
